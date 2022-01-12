96c276d2e179b511d6faeec64c9377a5922aa9ed
Khaoula866 <khaoulaaissaoui58@gmail.com>
2022-01-12 18:21:17 +0100

    Commit 6

--- PotfolioReact-Portfolio-Project/src/components/Home/Home2K.js
+++ PotfolioReact-Portfolio-Project/src/components/Home/Home2K.js
@@ -16,54 +16,53 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
-            </h1>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF{" "}
+            </h1>{" "}
             <p className="home-about-body">
               I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
+              something, I think…🤷‍♂️ <br />
+              <br /> I am fluent in classics like{" "}
               <i>
-                <b className="purple"> C#, Javascript and Python. </b>
-              </i>
+                <b className="purple"> C#, Javascript and Python. </b>{" "}
+              </i>{" "}
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My field of Interest 's are building new &nbsp;{" "}
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple"> Web Technologies and Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
-                </b>
-              </i>
+                  Deep Learning and Natural Launguage Processing.{" "}
+                </b>{" "}
+              </i>{" "}
               <br />
               <br />
               Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              with <b className="purple"> Node.js </b> and{" "}
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
+                  Modern Javascript Library and Frameworks{" "}
+                </b>{" "}
+              </i>{" "}
+              & nbsp; like{" "}
               <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
-            </p>
-          </Col>
+                <b className="purple"> React.js and Next.js </b>{" "}
+              </i>{" "}
+            </p>{" "}
+          </Col>{" "}
           <Col md={4} className="myAvtar">
             <Tilt>
               <img src={myImg} className="img-fluid" alt="avatar" />
-            </Tilt>
-          </Col>
-        </Row>
+            </Tilt>{" "}
+          </Col>{" "}
+        </Row>{" "}
         <Row>
           <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
+            <h1> FIND ME ON </h1>{" "}
             <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
+              Feel free to <span className="purple"> connect </span>with me{" "}
+            </p>{" "}
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
@@ -73,18 +72,18 @@ function Home2() {
                   className="icon-colour  home-social-icons"
                 >
                   <AiFillGithub />
-                </a>
-              </li>
+                </a>{" "}
+              </li>{" "}
               <li className="social-icons">
                 <a
-                  
+                  href="https://twitter.com/KhaOula66595432"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
                 >
                   <AiOutlineTwitter />
-                </a>
-              </li>
+                </a>{" "}
+              </li>{" "}
               <li className="social-icons">
                 <a
                   href="https://www.linkedin.com/in/khaoula-a-4341671a5/"
@@ -93,22 +92,22 @@ function Home2() {
                   className="icon-colour  home-social-icons"
                 >
                   <FaLinkedinIn />
-                </a>
-              </li>
+                </a>{" "}
+              </li>{" "}
               <li className="social-icons">
                 <a
-                  
+                  href="https://www.instagram.com/khaoulaell19/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
                 >
                   <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
+                </a>{" "}
+              </li>{" "}
+            </ul>{" "}
+          </Col>{" "}
+        </Row>{" "}
+      </Container>{" "}
     </Container>
   );
 }
